61c3b456487859ce2f39bc652ce96c87d2c6184a
Vjs Pranavasri <pranavasri@live.in>
2021-06-29 02:22:57 +0530

    Testing contents of event

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify(event.body),
+        body: JSON.stringify(event),
     };
     return response;
 };
