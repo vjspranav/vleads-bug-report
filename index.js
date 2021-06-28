bc720d48a5f541748e04fbaa0fe68ebafa619a9e
Vjs Pranavasri <pranavasri@live.in>
2021-06-29 02:11:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda, Deployed from github!'),
+        body: JSON.stringify(event.body),
     };
     return response;
 };
