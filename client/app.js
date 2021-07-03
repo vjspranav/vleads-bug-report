2def0aae0804156d78c5aa24a8e7101c704a2dbf
vjspranav <pranavasri@live.in>
2021-07-04 03:31:18 +0530

    Add support  for ss in front end * Test b64 created by front end to post to backend

--- client/app.js
+++ client/app.js
@@ -1,9 +1,41 @@
-const add_css = () => {
+"use_strict";
+
+const modal_div = document.createElement("div");
+const image_container = document.createElement("div");
+let b64 = "";
+
+const add_deps = () => {
   const link = document.createElement("link");
   link.rel = "stylesheet";
   link.type = "text/css";
-  link.href = "client/button.css";
-  document.getElementsByTagName("HEAD")[0].appendChild(link);
+  link.href = "client/bug-report.css";
+  document.head.appendChild(link);
+  const script = document.createElement("script");
+  script.src = "./client/html2canvas.js";
+  document.head.appendChild(script);
+};
+
+const add_modal_box = () => {
+  modal_div.classList += "modal";
+  document.getElementById("bug-report").appendChild(modal_div);
+};
+
+const populate_modal = () => {
+  const modal_content = document.createElement("div");
+  modal_content.classList += "modal-content";
+  const close_button = document.createElement("span");
+  close_button.innerHTML = "&times;";
+  close_button.classList += "close";
+  close_button.onclick = () => {
+    modal_div.style.display = "none";
+  };
+  let data = document.createElement("p");
+  data.innerHTML = "This will be your bug report";
+  modal_content.appendChild(close_button);
+  modal_content.appendChild(data);
+  modal_div.appendChild(modal_content);
+  image_container.id = "image-cotainer";
+  modal_content.appendChild(image_container);
 };
 
 const add_button = () => {
@@ -12,7 +44,17 @@ const add_button = () => {
   button.id = "bug-report-button";
   button.innerHTML = "Report Bug";
   button_div.appendChild(button);
+  button.onclick = () => {
+    html2canvas(document.body).then(function (canvas) {
+      image_container.appendChild(canvas);
+      let dataURL = canvas.toDataURL();
+      b64 = dataURL.split(",")[1];
+    });
+    modal_div.style.display = "block";
+  };
 };
 
-add_css();
+add_deps();
+add_modal_box();
 add_button();
+populate_modal();
