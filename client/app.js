f713e6ae25f7d0e515435bffdbfe5b83e2687c72
vjspranav <pranavasri@live.in>
2021-07-05 02:18:20 +0530

    Optionally include screenshot

--- client/app.js
+++ client/app.js
@@ -3,6 +3,60 @@
 const modal_div = document.createElement("div");
 const image_container = document.createElement("div");
 let b64 = "";
+let lab_data = {};
+
+const submit_bug_report = (label, labname, phase, expname, img = false) => {
+  console.log(
+    "Submitting bug report: \nlabel: " +
+      label +
+      "\nlabname: " +
+      labname +
+      "\nphase: " +
+      phase +
+      "\nexpname: " +
+      expname +
+      "\nimg: " +
+      (img ? true : img)
+  );
+};
+
+const get_lab_data = () => {
+  lab_data["labName"] = dataLayer[0]["labName"];
+  lab_data["label"] = dataLayer[0]["college"];
+  lab_data["phase"] = dataLayer[0]["phase"];
+  lab_data["expName"] = dataLayer[0]["expName"];
+};
+
+const create_checkbox = () => {
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.name = "image-checkbox";
+  checkbox.value = "false";
+  checkbox.id = "chkbox";
+
+  const label = document.createElement("label");
+  label.htmlFor = "chkbox";
+  label.appendChild(document.createTextNode("Add image to bug report"));
+  return [label, checkbox];
+};
+
+const create_button = () => {
+  const button = document.createElement("button");
+  button.id = "submit";
+  button.classList += "button";
+  button.innerHTML = "Submit";
+  button.onclick = () => {
+    let checkbox = document.getElementById("chkbox");
+    submit_bug_report(
+      lab_data["label"],
+      lab_data["labName"],
+      lab_data["phase"],
+      lab_data["experiment"],
+      checkbox.checked ? b64 : false
+    );
+  };
+  return button;
+};
 
 const add_deps = () => {
   const link = document.createElement("link");
@@ -26,6 +80,7 @@ const populate_modal = () => {
   const modal_content = document.createElement("div");
   modal_content.classList += "modal-content";
   const close_button = document.createElement("span");
+  const [checkbox, label] = create_checkbox();
   close_button.innerHTML = "&times;";
   close_button.classList += "close";
   close_button.onclick = () => {
@@ -36,8 +91,11 @@ const populate_modal = () => {
   modal_content.appendChild(close_button);
   modal_content.appendChild(data);
   modal_div.appendChild(modal_content);
+  modal_content.appendChild(checkbox);
+  modal_content.appendChild(label);
   image_container.id = "image-cotainer";
   modal_content.appendChild(image_container);
+  modal_content.appendChild(create_button());
 };
 
 const add_button = () => {
@@ -47,13 +105,30 @@ const add_button = () => {
   button.innerHTML = "Report Bug";
   button_div.appendChild(button);
   button.onclick = () => {
-    html2canvas(document.body).then(function (canvas) {
+    var canvas = document.createElement("canvas");
+    // canvas.scale = 0.3;
+    var opts = {
+      // canvas: canvas,
+      logging: true,
+      useCORS: true,
+    };
+    html2canvas(document.body, opts).then(function (canvas) {
+      canvas.id = "image-canva";
+      image_container.innerHTML = "";
       image_container.appendChild(canvas);
       let dataURL = canvas.toDataURL();
       b64 = dataURL.split(",")[1];
     });
     modal_div.style.display = "block";
+    get_lab_data();
+    console.log(lab_data);
   };
+};
+
+window.onclick = function (event) {
+  if (event.target == modal_div) {
+    modal_div.style.display = "none";
+  }
 };
 
 add_deps();
